3cacca7db3ca54657c4cf7b5445620c23188d64e
Kelly Steensma <kellysteensma@gmail.com>
2013-06-10 15:13:45 -0500

    Adding code to read text files.

--- src/Server.java
+++ src/Server.java
@@ -1,57 +1,68 @@
-
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
 
 public class Server {
-  OutputStream out;
+  private byte[] contentInBytes;
   private int port;
   private String directory;
-  private String startCommand;
-  public ServerSocket theServerSocket;
+  private byte[] headerInBytes;
+  private ServerSocket theServerSocket;
+  OutputStream out;
 
-  public Server(String port, String directory) {
+  public Server(byte[] content, String MIMEType, String port, String directory) throws UnsupportedEncodingException {
     this.port = Integer.parseInt(port);
     this.directory = directory;
+    this.contentInBytes = content;
+
+    String header = "HTTP/1.0 200 OK\r\n"
+        + "Server: KellyServer 1.0 \r\n"
+        + "Content-length: " + this.contentInBytes.length + "\r\n"
+        + "Content-type: " + MIMEType + "\r\n\r\n";
+
+    this.headerInBytes = header.getBytes("ASCII");
   }
 
   public static void main(String[] args) throws UnsupportedEncodingException {
+    try {
 
     int portIndex = Arrays.asList(args).indexOf("-p");
     int directoryIndex = Arrays.asList(args).indexOf("-d");
 
-    Server theServer = new Server(args[portIndex+1], args[directoryIndex+1]);
+    InputStream in = new FileInputStream("public/the_goal.txt");
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    int b;
+    while (( b = in.read()) != -1) out.write(b);
+    byte[] content = out.toByteArray();
+
+    Server theServer = new Server(content, "text/plain", args[portIndex+1], args[directoryIndex+1]);
     theServer.startServer();
+    }
+
+    catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Usage: java SingleFileHTTPServer filename port encoding");
+    }
+
+    catch (Exception e) {
+      System.err.println(e);
+    }
+
   }
 
   public void startServer() {
-
     try {
       theServerSocket = new ServerSocket(port);
       System.out.println("Server is running on port " + port + ".");
 
       while (true) {
-
         Socket theConnection = theServerSocket.accept();
-
         out = new BufferedOutputStream(theConnection.getOutputStream());
-
-        String data = "HTTP/1.0 200 OK\r\n"
-            + "Server: KellyServer 1.0 \r\n"
-            + "Content-length: 52\r\n"
-            + "Content-type: text/html\r\n\r\n"
-            + "<html><head></head><body>Hi, everyone!</body></html>";
-
-        byte[] dataInBytes = data.getBytes("ASCII");
-
-        out.write(dataInBytes);
+        out.write(headerInBytes);
+        out.write(contentInBytes);
         out.flush();
-
       }
+
     }
 
     catch (IOException e) {
