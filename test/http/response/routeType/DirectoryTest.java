f64743deed26ba9cd9fa7de79a022b3df3a314e0
Kelly Steensma <kellysteensma@gmail.com>
2013-08-20 15:55:01 -0500

    Deleted extra Directory test.

--- test/http/response/routeType/DirectoryTest.java
+++ test/http/response/routeType/DirectoryTest.java
@@ -12,7 +12,7 @@ public class DirectoryTest {
   private String NEW_LINE = "\r\n";
 
   @Test
-  public void buildRouteToSimpleFile() throws IOException {
+  public void buildRootDirectory() throws IOException {
     String workingDirectory = System.getProperty("user.dir");
     File publicDirectoryFullPath = new File(workingDirectory, "test/public/");
 
@@ -74,7 +74,7 @@ public class DirectoryTest {
   }
 
   @Test
-  public void buildRouteToFolder() throws IOException {
+  public void buildNestedFolderDirectory() throws IOException {
     String workingDirectory = System.getProperty("user.dir");
     File publicDirectoryFullPath = new File(workingDirectory, "test/public/");
 
@@ -113,51 +113,6 @@ public class DirectoryTest {
         + "    <a href=\"/images/404.png\">/404.png</a><br><a href=\"/images/another_404.png\">/another_404.png</a><br>\n"
         + "  </BODY>\n"
         + "</HTML>";
-    String expectedResult = expectedHeader + NEW_LINE + expectedBody;
-
-    assertEquals(expectedResult, actualResult);
-  }
-
-  @Test
-  public void buildRouteToFileInsideFolder() throws IOException {
-    String workingDirectory = System.getProperty("user.dir");
-    File publicDirectoryFullPath = new File(workingDirectory, "test/public/");
-
-    HashMap request = new HashMap();
-    request.put("httpMethod", "GET");
-    request.put("url", "/images");
-    request.put("httpProtocol", "HTTP/1.1");
-    request.put("Host", "localhost:5000");
-    request.put("Connection", "keep-alive");
-    request.put("Content-Length", "15");
-    request.put("Cache-Control", "max-age=0");
-    request.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-    request.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36");
-    request.put("Accept-Encoding", "gzip,deflate,sdch");
-    request.put("Accept-Language", "en-US,en;q=0.8");
-    request.put("Cookie", "textwrapon=false; wysiwyg=textarea");
-
-    File routeFile = new File(workingDirectory, "src/http/templates/file_directory.html");
-    Directory directory = new Directory(publicDirectoryFullPath);
-    byte[] actualResultInBytes = directory.get(routeFile, request);
-    String actualResult = new String(actualResultInBytes);
-
-    String expectedHeader = "HTTP/1.1 200 OK\r\n"
-        + new Date() + "\r\n"
-        + "Server: NinjaServer 1.0" + "\r\n"
-        + "Content-type: text/html; charset=UTF-8" + "\r\n"
-        + "Content-length: 231\r\n";
-    String expectedBody   = "<HTML>\n"
-                          + "  <HEAD>\n"
-                          + "    <TITLE>\n"
-                          + "      File Directory\n"
-                          + "    </TITLE>\n"
-                          + "  </HEAD>\n"
-                          + "  <BODY>\n"
-                          + "    <H1>/images Folder</H1>\n"
-                          + "    <a href=\"/images/404.png\">/404.png</a><br><a href=\"/images/another_404.png\">/another_404.png</a><br>\n"
-                          + "  </BODY>\n"
-                          + "</HTML>";
     String expectedResult = expectedHeader + NEW_LINE + expectedBody;
 
     assertEquals(expectedResult, actualResult);
