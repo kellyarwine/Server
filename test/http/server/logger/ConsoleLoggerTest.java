3732866aa27d8f4da8be2875b72aecad4deb0a70
Kelly Steensma <kellysteensma@gmail.com>
2013-09-22 03:00:39 -0500

    Redirection of system out was an issue; needed another redirect after testing complete; updating code to fix.

--- test/http/server/logger/ConsoleLoggerTest.java
+++ test/http/server/logger/ConsoleLoggerTest.java
@@ -8,22 +8,26 @@ import static junit.framework.Assert.assertEquals;
 
 public class ConsoleLoggerTest {
   private File consoleFile;
+  private PrintStream originalSystemOut;
 
   @Before
   public void setUp() throws IOException {
     String workingDirectory = System.getProperty("user.dir");
     consoleFile = new File(workingDirectory + "/console.txt");
+
+    System.setOut(new PrintStream(consoleFile));
+    originalSystemOut = new PrintStream(System.out);
   }
 
   @After
   public void tearDown() {
     consoleFile.delete();
+    System.setOut(new PrintStream(System.out));
   }
 
   @Test
   public void printMultipleMessagesToConsole() throws IOException {
     Logger logger = new ConsoleLogger();
-    System.setOut(new PrintStream(consoleFile));
     logger.logMessage("This message should be printed to the console.");
     logger.logMessage("This message should also be printed to the console.");
 
