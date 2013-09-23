ad46f6333e84a0e50704d4b18e093c5bd107917d
Kelly Steensma <kellysteensma@gmail.com>
2013-09-23 15:28:44 -0500

    Realizing I don't need method to create a new log file.  The FileOutputStream handles this.

--- src/http/server/logger/FileLogger.java
+++ src/http/server/logger/FileLogger.java
@@ -3,27 +3,19 @@ package http.server.logger;
 import java.io.*;
 
 public class FileLogger extends Logger {
-  private File workingDirectory;
   private String SERVER_LOG = "server.log";
+  private File workingDirectory;
 
-  public FileLogger(File workingDirectory) {
+  public FileLogger(File workingDirectory) throws IOException {
     this.workingDirectory = workingDirectory;
   }
 
   public void out(String message) throws IOException {
-    File logFile = findOrCreateLogFile();
+    File logFile = new File(workingDirectory, SERVER_LOG);
     FileOutputStream fos = new FileOutputStream(logFile, true);
     OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos, "utf-8");
     Writer writer = new BufferedWriter(outputStreamWriter);
     writer.append(message + "\n");
     writer.close();
-  }
-
-  private File findOrCreateLogFile() throws IOException {
-    File logFile = new File(workingDirectory, SERVER_LOG);
-    if (!logFile.exists()) {
-      logFile.createNewFile();
-    }
-    return logFile;
   }
 }
