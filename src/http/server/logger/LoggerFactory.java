47efaf393f7bea2ed5497ecedfff70f1a4839909
Kelly Steensma <kellysteensma@gmail.com>
2013-09-23 15:28:10 -0500

    Small refactoring.

--- src/http/server/logger/LoggerFactory.java
+++ src/http/server/logger/LoggerFactory.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class LoggerFactory {
   public static Logger build(String env, File workingDirectoryFullPath) throws IOException {
     if (env.equals("production"))
-        return new ConsoleLogger();
+      return new ConsoleLogger();
     else {
       return new FileLogger(workingDirectoryFullPath);
     }
