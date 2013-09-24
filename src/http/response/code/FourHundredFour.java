3c6f1e4e09a0f1dc816a784845e6936843e4e7bc
Kelly Steensma <kellysteensma@gmail.com>
2013-09-24 16:28:39 -0500

    Moved response classes to separate project - ResponseBuilder.

--- src/http/response/code/FourHundredFour.java
+++
@@ -1,7 +0,0 @@
-package http.response.code;
-
-public class FourHundredFour extends Code {
-  public String responseCodeMessage() {
-    return "404 File Not Found";
-  }
-}
--- src/http/response/code/TwoHundred.java
+++
@@ -1,7 +0,0 @@
-package http.response.code;
-
-public class TwoHundred extends Code {
-  public String responseCodeMessage() {
-    return "200 OK";
-  }
-}
