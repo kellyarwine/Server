e1e7040fe8e4a7af5f18f9e9e25970335909fedc
Kelly Steensma <kellysteensma@gmail.com>
2013-09-25 23:08:42 -0500

    Deleted Main and Server classes since server is now started in Router .jars.

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import http.io.SystemIo;
-import http.server.Server;
-
-public class Main {
-  public static void main(String[] args) throws Exception {
-    new Server(new SystemIo()).initialize();
-  }
-}
